2b62e3752b61cc69bbe2cd6e2c3fd35dd3db15e9
Robert Pack <robert.pack@basf.com>
2020-09-02 07:11:27 +0200

    remove some unnecessary code

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,20 @@
 import * as React from "react";
-import {
-  Checkbox,
-  ColorPicker,
-  createTheme,
-  Customizer,
-  Dropdown,
-  Fabric,
-  initializeIcons,
-  PrimaryButton,
-  Slider,
-  TextField,
-  Toggle,
-  IDropdownStyles,
-} from "office-ui-fabric-react";
+import { Dropdown, Fabric, initializeIcons } from "office-ui-fabric-react";
 
-initializeIcons();
-
-// Go to aka.ms/themedesigner for more control over theme.
-const theme = createTheme({
-  palette: {
-    themePrimary: "red",
-  },
-});
+initializeIcons()
 
 const dropdownStyles = { root: { width: 200 } };
-
 const dropdownObjects = [];
-
 for (let i = 0; i < 100; i++) {
   dropdownObjects.push({ key: `object${i}`, text: `object${i}` });
 }
 
 const Index = () => (
-  <Customizer settings={{ theme }}>
-    <Fabric applyTheme>
-      <div>
-        <Dropdown styles={dropdownStyles} options={dropdownObjects} />
-      </div>
-    </Fabric>
-  </Customizer>
+  <Fabric>
+    <div>
+      <Dropdown styles={dropdownStyles} options={dropdownObjects} />
+    </div>
+  </Fabric>
 );
 
 export default Index;
